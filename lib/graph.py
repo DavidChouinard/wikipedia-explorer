dd28fe03c63277dd30f2f35d8911387163d275d4
David Chouinard <david@davidchouinard.com>
2013-04-12 21:19:27 -0400

    Initial graph represetation implementation

---
+++ lib/graph.py
@@ -0,0 +1,11 @@
+class Graph:
+    def __init__(self, head):
+        self.head = head
+
+class Graph:
+    def __init__(self, data, adjacent):
+        self.data = data
+        self.adjacent = adjacent
+
+    #TODO: string representation
+    #def __str__(self):
