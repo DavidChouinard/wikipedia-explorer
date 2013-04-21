ae457db57fff28608d1f8d2101f814ed58b6039b
David Chouinard <david@davidchouinard.com>
2013-04-21 11:31:06 -0400

    Working recursive crawler

--- crawler.py
+++ crawler.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from lib.graph import *
-import io, argparse, urllib, urllib2, json
+import io, argparse, urllib, urllib2, json, pickle
 import pprint
+
+TOPLEVEL_CATEGORY = "Category:History of computer science"
+TOPLEVEL_CATEGORY_ID = 30730499
 
 endpoint = 'http://en.wikipedia.org/w/api.php'
 parameters = {'format' : 'json',
@@ -12,7 +15,6 @@ parameters = {'format' : 'json',
               'prop' : 'links|categories',
               'cllimit' : 500,
               'pllimit' : 500,
-              'pllimit' : 10,
               'plnamespace' : 0,
               #'prop' : 'extracts',
               #'exsectionformat' : 'raw',
@@ -20,30 +22,23 @@ parameters = {'format' : 'json',
               'redirects' : True}
 
 crawled_articles = []
-#categories = set([691117])
-categories = set([33240744])
+categories = set([TOPLEVEL_CATEGORY])
 
 def main(args):
-    # TODO: create list of category
+    get_child_categories(TOPLEVEL_CATEGORY_ID)
 
-    for category in categories:
-        getChildCategories(category)
+    graph = Graph(crawl(args.start))
 
-    print len(categories)
-
-    #graph = Graph(crawl(args.start))
-
-    #pickle.dump(graph, open('data/wikipedia.pkl', 'wb'), -1)
+    pickle.dump(graph, open('data/wikipedia.pkl', 'wb'), -1)
 
 def crawl(title):
-    #print title
     parameters["titles"] = title
     crawled_articles.append(title)
 
-    response = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(parameters))))
+    response = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(encode_dict(parameters)))))
     page = response[u'query'][u'pages'].values()[0]
 
-    pprint.pprint(response)
+    #pprint.pprint(response)
 
     if u'missing' in page:
         return None
@@ -51,16 +46,24 @@ def crawl(title):
         adjacent_nodes = []
 
         for link in page[u'links']:
-            if link[u'title'] not in crawled_articles:
+            print link[u'title']
+            if link[u'title'] not in crawled_articles and is_in_categories(page[u'categories']):
+                print "ACCEPTED"
                 node = crawl(link[u'title'])
                 if node:
                     adjacent_nodes.append(crawl(link[u'title']))
+            else:
+                print "REJECTED"
 
         return Node({"title": page[u'title'] , "id": page[u'pageid']}, adjacent_nodes)
 
+def is_in_categories(page):
+    for category in page:
+        if category[u'title'] in categories:
+            return True
+    return False
 
-def getChildCategories(category_id):
-    #print category_id
+def get_child_categories(category_id):
     query_parameters = {'format': 'json',
                         'action': 'query',
                         'list': 'categorymembers',
@@ -72,10 +75,21 @@ def getChildCategories(category_id):
     #pprint.pprint(response)
 
     for category in response[u'query'][u'categorymembers']:
-        if category[u'pageid'] not in categories:
-            print category[u'title']
-            categories.add(category[u'pageid'])
-            getChildCategories(category[u'pageid'])
+        if category[u'title'] not in categories:
+            #print category[u'title']
+            categories.add(category[u'title'])
+            get_child_categories(category[u'pageid'])
+
+def encode_dict(in_dict):
+    out_dict = {}
+    for k, v in in_dict.iteritems():
+        if isinstance(v, unicode):
+            v = v.encode('utf8')
+        elif isinstance(v, str):
+            # Must be encoded in UTF-8
+            v.decode('utf8')
+        out_dict[k] = v
+    return out_dict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
