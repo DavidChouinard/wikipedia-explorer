a56a2f50cb27960d39de801ec0ec430c0e9238d4
David Chouinard <david@davidchouinard.com>
2013-04-23 17:00:26 -0400

    Updated crawler to only return links in article summary

--- crawler.py
+++ crawler.py
@@ -4,10 +4,10 @@ from lib.graph import *
 import io, os, argparse, urllib, urllib2, json, pickle
 import pprint
 
-TOPLEVEL_CATEGORY = "Category:History of computer science"
-TOPLEVEL_CATEGORY_ID = 30730499
+TOPLEVEL_CATEGORY = "Category:Computer science"
+TOPLEVEL_CATEGORY_ID = 691117
 
-CATEGORY_PATH = 'data/categories.pkl'
+#CATEGORY_PATH = 'data/categories.pkl'
 
 endpoint = 'http://en.wikipedia.org/w/api.php'
 parameters = {'format' : 'json',
@@ -17,15 +17,15 @@ parameters = {'format' : 'json',
               'redirects' : True}
 
 crawled_articles = []
-restrict_categories = set([TOPLEVEL_CATEGORY])
+#restrict_categories = set([TOPLEVEL_CATEGORY])
 
 def main(args):
-    global restrict_categories
-    if os.path.exists(CATEGORY_PATH):
-        restrict_categories = pickle.load(open(CATEGORY_PATH, 'rb'))
-    else:
-        get_child_categories(TOPLEVEL_CATEGORY_ID)
-        pickle.dump(restrict_categories, open(CATEGORY_PATH, 'wb'), -1)
+    #global restrict_categories
+    #if os.path.exists(CATEGORY_PATH):
+        #restrict_categories = pickle.load(open(CATEGORY_PATH, 'rb'))
+    #else:
+        #get_child_categories(TOPLEVEL_CATEGORY_ID)
+        #pickle.dump(restrict_categories, open(CATEGORY_PATH, 'wb'), -1)
 
     graph = Graph(crawl(args.start))
 
@@ -39,7 +39,7 @@ def crawl(title):
 
     adjacent_nodes = []
     for link in response[u'links']:
-        if link[u'*'] not in crawled_articles and link[u'ns'] == 0 and link[u'exists'] == u'':
+        if link[u'*'] not in crawled_articles and link[u'ns'] == 0 and u'exists' in link:
             print link[u'*']
             query_parameters = {'format': 'json',
                                 'action': 'query',
@@ -48,15 +48,19 @@ def crawl(title):
                                 'cllimit': 500}
             categories = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(query_parameters))))[u'query'][u'pages'].values()[0]
 
-            if u'categories' in categories and is_in_categories(categories[u'categories']):
-                print "ACCEPTED"
+            if u'categories' in categories and is_computer_science(categories[u'categories']):
                 node = crawl(link[u'*'])
                 if node:
                     adjacent_nodes.append(crawl(link[u'*']))
-            else:
-                print "REJECTED"
 
     return Node({"title": response[u'title']}, adjacent_nodes)
+
+def is_computer_science(categories):
+    for category in categories:
+        if ("comput" in category[u'title'].lower() or "software" in category[u'title'].lower()
+                or "program" in category[u'title'].lower() or "cybet" in category[u'title'].lower() ):
+            return True
+    return False
 
 def is_in_categories(categories):
     for category in categories:
@@ -74,24 +78,11 @@ def get_child_categories(category_id):
                         'cmlimit': 500}
     response = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(query_parameters))))
 
-    #pprint.pprint(response)
-
     for category in response[u'query'][u'categorymembers']:
         if category[u'title'] not in restrict_categories:
-            #print category[u'title']
+            print category[u'title']
             restrict_categories.add(category[u'title'])
             get_child_categories(category[u'pageid'])
-
-def encode_dict(in_dict):
-    out_dict = {}
-    for k, v in in_dict.iteritems():
-        if isinstance(v, unicode):
-            v = v.encode('utf8')
-        elif isinstance(v, str):
-            # Must be encoded in UTF-8
-            v.decode('utf8')
-        out_dict[k] = v
-    return out_dict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
