c3f1eb9cf7e013d52ecd3445b6aed591b11674c5
David Chouinard <david@davidchouinard.com>
2013-04-29 15:22:53 -0400

    Updated crawler per representation change

--- crawler.py
+++ crawler.py
@@ -1,95 +1,64 @@
 # -*- coding: utf-8 -*-
 
-from lib.graph import *
 import io, os, argparse, urllib, urllib2, json, pickle
-import pprint
-
-#TOPLEVEL_CATEGORY = "Category:Computer science"
-#TOPLEVEL_CATEGORY_ID = 691117
-#CATEGORY_PATH = 'data/categories.pkl'
-MAX_DEPTH = 250
+#import pprint
 
 endpoint = 'http://en.wikipedia.org/w/api.php'
-parameters = {'format' : 'json',
-              'action' : 'parse',
-              'prop' : 'links',
-              'section' : 0,
-              'redirects' : True}
 
-crawled_articles = []
-#restrict_categories = set([TOPLEVEL_CATEGORY])
+crawled_categories = set()
+graph = dict()
 
 def main(args):
-    #global restrict_categories
-    #if os.path.exists(CATEGORY_PATH):
-        #restrict_categories = pickle.load(open(CATEGORY_PATH, 'rb'))
-    #else:
-        #get_child_categories(TOPLEVEL_CATEGORY_ID)
-        #pickle.dump(restrict_categories, open(CATEGORY_PATH, 'wb'), -1)
+    # Make sure the passed category has "Category:" in front of it
+    if args.category[:9] == "Category:":
+        get_subcat_articles(args.category)
+    else:
+        get_subcat_articles("Category:" + args.category)
 
-    graph = Graph(crawl(args.start))
+
+    parameters = {'format' : 'json',
+                  'action' : 'parse',
+                  'prop' : 'links',
+                  'section' : 0,
+                  'redirects' : True}
+
+    for title, adjacent in graph.iteritems():
+        parameters["page"] = title.encode('utf8')
+        response = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(parameters))))[u'parse']
+
+        print u'Adding adjacent nodes for article ' + title
+        for link in response[u'links']:
+            if link[u'ns'] == 0 and u'exists' in link:
+                adjacent.add(link[u'*'])
+        #pprint.pprint(graph)
 
     pickle.dump(graph, open('data/wikipedia.pkl', 'wb'), -1)
 
-def crawl(title, depth = 1):
+def get_subcat_articles(category):
+    if category in crawled_categories:
+        return
+    else:
+        crawled_categories.add(category)
 
-    if depth >= MAX_DEPTH:
-        return None
-
-    parameters["page"] = title.encode('utf8')
-    crawled_articles.append(title)
-
-    response = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(parameters))))[u'parse']
-
-    adjacent_nodes = []
-    for link in response[u'links']:
-        if link[u'*'] not in crawled_articles and link[u'ns'] == 0 and u'exists' in link:
-            query_parameters = {'format': 'json',
-                                'action': 'query',
-                                'prop': 'categories',
-                                'titles': link[u'*'].encode('utf8'),
-                                'cllimit': 500}
-            categories = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(query_parameters))))[u'query'][u'pages'].values()[0]
-            if u'categories' in categories and is_computer_science(categories[u'categories']):
-                #print link[u'*'] + "  " + str(depth)
-                print link[u'*']
-                node = crawl(link[u'*'], depth + 1)
-                if node:
-                    adjacent_nodes.append(node)
-
-    return Node({"title": response[u'title']}, adjacent_nodes)
-
-def is_computer_science(categories):
-    for category in categories:
-        if ("comput" in category[u'title'].lower() or "software" in category[u'title'].lower() or "program" in category[u'title'].lower()):
-            return True
-    return False
-
-def is_in_categories(categories):
-    for category in categories:
-        if category[u'title'] in restrict_categories:
-            return True
-    return False
-
-def get_child_categories(category_id):
-    global restrict_categories
     query_parameters = {'format': 'json',
                         'action': 'query',
                         'list': 'categorymembers',
-                        'cmpageid': category_id,
-                        'cmtype': 'subcat',
+                        'cmtitle': category.encode('utf8'),
+                        #'cmtype': 'subcat',
                         'cmlimit': 500}
     response = json.load(urllib2.urlopen(urllib2.Request(endpoint, urllib.urlencode(query_parameters))))
 
-    for category in response[u'query'][u'categorymembers']:
-        if category[u'title'] not in restrict_categories:
-            print category[u'title']
-            restrict_categories.add(category[u'title'])
-            get_child_categories(category[u'pageid'])
+    for member in response[u'query'][u'categorymembers']:
+        if member[u'ns'] == 0:
+            if member[u'title'] not in graph:
+                graph[member[u'title']] = set()
+        elif member[u'ns'] == 14:
+            print u'Fetching articles from ' + member[u'title']
+            get_subcat_articles(member[u'title'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("start", help="initial article to start crawling")
+    parser.add_argument("category", help="category to restrict to graph to")
     args = parser.parse_args()
 
     main(args)
