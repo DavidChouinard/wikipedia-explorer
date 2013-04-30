c9f4944b61e421e90dfa687f0f2b44f4add9d663
David Chouinard <david@davidchouinard.com>
2013-04-30 16:25:24 -0400

    Reverted to lists for mock data structure

--- mock.py
+++ mock.py
@@ -4,10 +4,10 @@ import pickle
 
 def main():
     graph = {
-        "Computer science": set(["Computational complexity theory", "Theory of computation", "Abstract algebra"]),
-        "Computational complexity theory": set(["Complexity theory", "Lambda calculus"]),
-        "Abstract algebra": set(["Von Newman"]),
-        "Theory of computation": set()
+        "Computer science": ["Computational complexity theory", "Theory of computation", "Abstract algebra"],
+        "Computational complexity theory": ["Complexity theory", "Lambda calculus"],
+        "Abstract algebra": ["Von Newman"],
+        "Theory of computation": []
     }
 
     pickle.dump(graph, open('data/mock.pkl', 'wb'), -1)
