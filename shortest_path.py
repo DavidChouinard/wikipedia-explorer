15bef99f839caa2e95fe484ccfed18d36bf02302
Matthew J. Holman <mholman@cfa.harvard.edu>
2013-05-01 20:56:05 -0400

    latest

--- shortest_path.py
+++ shortest_path.py
@@ -6,8 +6,24 @@ import io, argparse, pickle
 def main(args):
     graph = pickle.load(open(args.data, 'rb'))
 
+    #head = graph.head
+    #head.data["distance"] = 0
+    #head.data["visited"] = True
+
+    current_distance = 0
+    for child in head.adjacent:
+        child.data["distance"] = current_distance + 1
+        
+    # if "distance" in child.data
+    
+    # nodes have data and a list called adjacent
+
+    for child in head.adjacent:
+        print child.data
+        
+
     # TODO: Find the distance between args.source and args.destination
-    print "OMG"
+    print args.source, args.destination
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
