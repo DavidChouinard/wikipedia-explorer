fe58e0a3b758675f11fe95cc77f28eeba45b3343
Kelly Marchisio <kvm074@mail.harvard.edu>
2013-05-01 21:04:45 -0400

    message

---
+++ shortest_path3.py
@@ -0,0 +1,33 @@
+from lib.graph import *
+import io, argparse, pickle, pprint
+
+def main(args):
+    graph = pickle.load(open(args.data, 'rb'))
+   # pprint.pprint(graph)
+    find_shortest_path(graph,args.source,args.destination,[])
+
+# from http://www.python.org/doc/essays/graphs.html
+
+def find_shortest_path(graph, start, end, path):
+    path = path + [start]
+    if start == end:
+        print path
+        return path
+    if not start in graph:
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    # pprint.pprint(shortest)
+    return shortest
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data", help="data file containing the graph")
+    parser.add_argument("source", help="source article title")
+    parser.add_argument("destination", help="destination article title")
+    main(parser.parse_args())
