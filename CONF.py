f6557710efb97145cb10de64a0d18800b63ab3ed
Matthew J. Holman <mholman@cfa.harvard.edu>
2013-05-05 22:28:58 -0400

    latest

---
+++ CONF.py
@@ -0,0 +1,5 @@
+# Configuration values
+n_step_value = 3
+maxPathLength = 6
+maxRandomWeight = 10
+
---
+++ bfs.py
@@ -0,0 +1,24 @@
+from CONF import *
+# bfs found here : 
+#   http://stackoverflow.com/questions/8922060/breadth-first-search-trace-path
+def bfs(graph, start, end):
+    # maintain a queue of paths
+    queue = []
+    # push the first path into the queue
+    queue.append([start])
+    while queue:
+        # get the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        # path found
+        if node == end:
+            return path
+        if len(path) > maxPathLength:
+            return None
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        for adjacent in graph.get(node, []):
+            new_path = list(path)
+            new_path.append(adjacent)
+            queue.append(new_path)
+
